f43b5524cd1ba11d56aff73099c177ed613b6949
azminural <azmi.nural@setrow.com>
2022-05-22 20:09:47 +0300

    Projeye redux toolkit kuruldu

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import screens from "./src/screens";
+import store from "./src/store";
+import { Provider } from "react-redux";
 
+
+import screens from "./src/screens";
 const Stack = createNativeStackNavigator();
 
 
@@ -13,21 +16,26 @@ const App = () => {
         screenOptions={{
           headerShown: false,
         }}
-        >
+      >
         <Stack.Screen
           name="Home"
           component={screens.HomeScreen}
-          options={{ title: 'Home' }}
+          options={{ title: "Home" }}
         />
         <Stack.Screen
           name="List"
           component={screens.List}
-          options={{ title: 'List' }}
+          options={{ title: "List" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
+const ReduxProvider = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
-export default App;
+export default ReduxProvider;
---
+++ src/store/app.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const app = createSlice({
+  name: "app",
+  initialState: {
+
+  },
+  reducers: {
+
+  },
+});
+
+// Action creators are generated for each case reducer function
+// export const { setDeneme } = app.actions;
+
+export default app.reducer;
